1006b6103cb64100e697ae0a7b98f128a3e35ae3
Viljamas S. <141546611+viljamas-git@users.noreply.github.com>
2026-04-08 09:44:12 +0100

    Add files via upload

---
+++ vendor/railsware/mailtrap-php/src/MailtrapSendingClient.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mailtrap;
+
+/**
+ * @method  Api\Sending\Emails      emails()
+ * @method  Api\Sending\Suppression suppressions(int $accountId)
+ * @method  Api\Sending\Domain      domains(int $accountId)
+ * @method  Api\Sending\Stats       stats(int $accountId)
+ * @method  Api\Sending\EmailLogs  emailLogs(int $accountId)
+ *
+ * Class MailtrapSendingClient
+ */
+final class MailtrapSendingClient extends AbstractMailtrapClient implements EmailsSendMailtrapClientInterface
+{
+    public const API_MAPPING = [
+        'emails' => Api\Sending\Emails::class,
+        'suppressions' => Api\Sending\Suppression::class,
+        'domains' => Api\Sending\Domain::class,
+        'stats' => Api\Sending\Stats::class,
+        'emailLogs' => Api\Sending\EmailLogs::class,
+    ];
+}
